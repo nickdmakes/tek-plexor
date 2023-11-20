404f9eb887584636a01d8b20444027e51acea445
Nick Matthews <nickd.mf7@gmail.com>
2023-11-20 17:25:29 -0600

    spiked out download

--- tp_engine/yt_api.py
+++ tp_engine/yt_api.py
@@ -6,6 +6,24 @@ class YTAudioDownloadException(Exception):
     pass
 
 
+class YTTitleRetrievalException(Exception):
+    """Raised when there's a problem retrieving a video title"""
+    pass
+
+
+# get the title of a YouTube video
+def get_title(url: str):
+    try:
+        yt = YouTube(url)
+        info = yt.vid_info
+        title = info['videoDetails']['title']
+        artist = info['videoDetails']['author']
+        full_title = artist + ' - ' + title
+        return full_title
+    except Exception as e:
+        raise YTAudioDownloadException(e)
+
+
 def download_single_audio(url: str, file_name: str, out_path="./"):
     try:
         yt = YouTube(url)
--- tp_interface/app.py
+++ tp_interface/app.py
@@ -1,24 +1,24 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
 from .main_window_ui import Ui_MainWindow
+from tp_interface.YtSingleController import YtSingleController
 
 
 # show the main window
 def start_app():
     app = QApplication(sys.argv)
-    window = Window()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec())
 
 
-class Window(QMainWindow, Ui_MainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-        self.connectSignalsSlots()
+        self.ytSingleController = YtSingleController(self)
+        # state variables
+        self.downloading = False
 
-    def connectSignalsSlots(self):
-        self.downloadButton.clicked.connect(self.downloadButtonClicked)
-
-    def downloadButtonClicked(self):
-        self.statusbar.showMessage("Downloading...")
+    def setDownloading(self, new_state: bool = False):
+        self.downloading = new_state
