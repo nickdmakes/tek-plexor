cd07683a3124a7cf061f5199aae2bb35d4678a40
Nick Matthews <nickd.mf7@gmail.com>
2023-11-24 20:47:22 -0600

    on my grind

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-import os
-from tp_engine.yt_api import download_single_audio
-from tp_conversion.converter import convert_to_m4a
 from tp_interface.app import start_app
-
-url = "https://www.youtube.com/watch?v=VWoIpDVkOH0"
 
 
 def main():
-    # out_file = download_single_audio(url, out_path="data", file_name="test.opus")
-    # print(out_file)
-    # convert_to_m4a(out_file, out_file.replace(".opus", ".m4a"), delete_in_file=True)
-
     start_app()
 
 
